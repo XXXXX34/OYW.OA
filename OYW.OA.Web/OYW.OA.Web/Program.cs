faaac0d35af3e47f3f9cf1472f647bf7e92e46ca
XXXXX34 <503025565@qq.com>
2018-06-23 21:54:24 +0800

    功能优化

--- OYW.OA.Web/OYW.OA.Web/Program.cs
+++ OYW.OA.Web/OYW.OA.Web/Program.cs
@@ -19,6 +19,7 @@ namespace OYW.OA.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:5000")
                 .UseStartup<Startup>();
     }
 }
