5b8a90d44b6fc6fb7c785517c74e17d8e5e5e5c3
XXXXX34 <503025565@qq.com>
2018-06-23 10:47:06 +0800

    添加项目OYW.OA.Web

---
+++ OYW.OA.Web/OYW.OA.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OYW.OA.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
