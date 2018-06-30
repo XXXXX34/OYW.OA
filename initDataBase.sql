f4faa8f4f57ddda69f726f5818a88ca33fed4a54
XXXXX34 <503025565@qq.com>
2018-06-30 15:27:05 +0800

    数据库-更新

--- initDataBase.sql
+++ initDataBase.sql
@@ -1,11 +1,62 @@
 ﻿USE [OYW.OA]
 GO
-/****** Object:  Table [dbo].[ORG_User]    Script Date: 2018/6/24 11:42:26 ******/
+EXEC sys.sp_dropextendedproperty @name=N'MS_Description' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SYS_Menu', @level2type=N'COLUMN',@level2name=N'CreateTime'
+
+GO
+EXEC sys.sp_dropextendedproperty @name=N'MS_Description' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SYS_Menu', @level2type=N'COLUMN',@level2name=N'DeptName'
+
+GO
+EXEC sys.sp_dropextendedproperty @name=N'MS_Description' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SYS_Menu', @level2type=N'COLUMN',@level2name=N'DeptID'
+
+GO
+EXEC sys.sp_dropextendedproperty @name=N'MS_Description' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SYS_Menu', @level2type=N'COLUMN',@level2name=N'EmplName'
+
+GO
+EXEC sys.sp_dropextendedproperty @name=N'MS_Description' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SYS_Menu', @level2type=N'COLUMN',@level2name=N'EmplID'
+
+GO
+EXEC sys.sp_dropextendedproperty @name=N'MS_Description' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SYS_Menu', @level2type=N'COLUMN',@level2name=N'Sort'
+
+GO
+EXEC sys.sp_dropextendedproperty @name=N'MS_Description' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SYS_Menu', @level2type=N'COLUMN',@level2name=N'ParentID'
+
+GO
+EXEC sys.sp_dropextendedproperty @name=N'MS_Description' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SYS_Menu', @level2type=N'COLUMN',@level2name=N'Action'
+
+GO
+EXEC sys.sp_dropextendedproperty @name=N'MS_Description' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SYS_Menu', @level2type=N'COLUMN',@level2name=N'Icon'
+
+GO
+EXEC sys.sp_dropextendedproperty @name=N'MS_Description' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SYS_Menu', @level2type=N'COLUMN',@level2name=N'MenuName'
+
+GO
+EXEC sys.sp_dropextendedproperty @name=N'MS_Description' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'SYS_Menu', @level2type=N'COLUMN',@level2name=N'ID'
+
+GO
+ALTER TABLE [dbo].[ORG_User] DROP CONSTRAINT [DF_ORG_User_UpdateTime]
+GO
+ALTER TABLE [dbo].[ORG_User] DROP CONSTRAINT [DF_ORG_User_CreateTime]
+GO
+ALTER TABLE [dbo].[ORG_User] DROP CONSTRAINT [DF_ORG_User_UserDisabled]
+GO
+ALTER TABLE [dbo].[ORG_User] DROP CONSTRAINT [DF_ORG_User_UserNeedChgPwd]
+GO
+/****** Object:  Table [dbo].[SYS_Menu]    Script Date: 2018/6/30 15:26:25 ******/
+DROP TABLE [dbo].[SYS_Menu]
+GO
+/****** Object:  Table [dbo].[ORG_UserLogon]    Script Date: 2018/6/30 15:26:25 ******/
+DROP TABLE [dbo].[ORG_UserLogon]
+GO
+/****** Object:  Table [dbo].[ORG_User]    Script Date: 2018/6/30 15:26:25 ******/
+DROP TABLE [dbo].[ORG_User]
+GO
+/****** Object:  Table [dbo].[ORG_User]    Script Date: 2018/6/30 15:26:25 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[ORG_User](
+	[ID] [uniqueidentifier] NOT NULL,
 	[UserName] [nvarchar](50) NOT NULL,
 	[UserPwd] [nvarchar](500) NULL,
 	[EmplID] [nvarchar](50) NULL,
@@ -17,12 +68,30 @@ CREATE TABLE [dbo].[ORG_User](
 	[UpdateTime] [datetime] NULL,
  CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
 (
-	[UserName] ASC
+	[ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
 GO
-/****** Object:  Table [dbo].[SYS_Menu]    Script Date: 2018/6/24 11:42:26 ******/
+/****** Object:  Table [dbo].[ORG_UserLogon]    Script Date: 2018/6/30 15:26:25 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ORG_UserLogon](
+	[ID] [uniqueidentifier] NOT NULL,
+	[UserID] [uniqueidentifier] NULL,
+	[IP] [nvarchar](50) NULL,
+	[Location] [nvarchar](50) NULL,
+	[LogonTime] [datetime] NULL,
+ CONSTRAINT [PK_ORG_EmplLogon] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[SYS_Menu]    Script Date: 2018/6/30 15:26:25 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -51,7 +120,21 @@ CREATE TABLE [dbo].[SYS_Menu](
 ) ON [PRIMARY]
 
 GO
-INSERT [dbo].[ORG_User] ([UserName], [UserPwd], [EmplID], [UserLogonTimes], [UserNeedChgPwd], [UserDisabled], [UserLastLogonIP], [CreateTime], [UpdateTime]) VALUES (N'admin', N'21232f297a57a5a743894ae4a801fc3', N'99999999-9999-9999-9999-999999999999', 0, 0, 0, N'127.0.0.1', CAST(0x0000A90800DF6A08 AS DateTime), CAST(0x0000A90800DF6A08 AS DateTime))
+INSERT [dbo].[ORG_User] ([ID], [UserName], [UserPwd], [EmplID], [UserLogonTimes], [UserNeedChgPwd], [UserDisabled], [UserLastLogonIP], [CreateTime], [UpdateTime]) VALUES (N'4a8f70a5-bd59-431c-bf71-addd1c5ee8bb', N'admin', N'21232f297a57a5a743894ae4a801fc3', N'99999999-9999-9999-9999-999999999999', 0, 0, 0, N'127.0.0.1', CAST(0x0000A90800DF6A08 AS DateTime), CAST(0x0000A90800DF6A08 AS DateTime))
+GO
+INSERT [dbo].[ORG_UserLogon] ([ID], [UserID], [IP], [Location], [LogonTime]) VALUES (N'd6e4fe49-97ef-443a-a701-0718230749aa', N'4a8f70a5-bd59-431c-bf71-addd1c5ee8bb', N'localhost:61630', N'', CAST(0x0000A90F00F390F5 AS DateTime))
+GO
+INSERT [dbo].[ORG_UserLogon] ([ID], [UserID], [IP], [Location], [LogonTime]) VALUES (N'ed29b420-d0c5-4bc4-9c81-3a020adc5368', N'4a8f70a5-bd59-431c-bf71-addd1c5ee8bb', N'localhost:61630', N'', CAST(0x0000A90F00D80C29 AS DateTime))
+GO
+INSERT [dbo].[ORG_UserLogon] ([ID], [UserID], [IP], [Location], [LogonTime]) VALUES (N'426435d7-6290-449c-b614-3a1e6b0e023c', N'4a8f70a5-bd59-431c-bf71-addd1c5ee8bb', N'localhost:61630', N'', CAST(0x0000A90F00D734EB AS DateTime))
+GO
+INSERT [dbo].[ORG_UserLogon] ([ID], [UserID], [IP], [Location], [LogonTime]) VALUES (N'af389923-cb34-46b1-91a0-3be4757b4080', N'4a8f70a5-bd59-431c-bf71-addd1c5ee8bb', N'localhost:61630', N'', CAST(0x0000A90F00F0C4A1 AS DateTime))
+GO
+INSERT [dbo].[ORG_UserLogon] ([ID], [UserID], [IP], [Location], [LogonTime]) VALUES (N'b10c9e3e-dd91-49d8-82d0-48224ad0676b', N'4a8f70a5-bd59-431c-bf71-addd1c5ee8bb', N'localhost:61630', N'', CAST(0x0000A90F00F174A5 AS DateTime))
+GO
+INSERT [dbo].[ORG_UserLogon] ([ID], [UserID], [IP], [Location], [LogonTime]) VALUES (N'33b8e526-82df-46c8-b79b-881b085322d6', N'4a8f70a5-bd59-431c-bf71-addd1c5ee8bb', N'localhost:61630', N'', CAST(0x0000A90F00D75718 AS DateTime))
+GO
+INSERT [dbo].[ORG_UserLogon] ([ID], [UserID], [IP], [Location], [LogonTime]) VALUES (N'05ebeb7b-50b3-4393-accc-cce1f028501a', N'4a8f70a5-bd59-431c-bf71-addd1c5ee8bb', N'localhost:61630', N'', CAST(0x0000A90F00D2A332 AS DateTime))
 GO
 INSERT [dbo].[SYS_Menu] ([ID], [MenuName], [Code], [Icon], [App], [Controller], [Action], [Params], [URL], [ParentID], [Sort], [EmplID], [EmplName], [DeptID], [DeptName], [CreateTime]) VALUES (N'cffde5eb-f366-4d11-9f69-11f966fe4bc6', N'职位', N'Position', N'glyphicon glyphicon-th-large', N'OA.People', N'Position', N'Index', NULL, N'/Position/Index?app=People&menu=Position', N'bb838c09-273a-4409-ac9b-376a606d218d', 3, N'', N'', N'', N'', CAST(0x0000A7B6016077F0 AS DateTime))
 GO
