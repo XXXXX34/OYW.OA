32f865f2dab1805a74fd23a2247d6c63ee2f8087
XXXXX34 <503025565@qq.com>
2018-07-14 22:21:47 +0800

    添加Exceptionless日志收集

---
+++ OYW.OA.ExceptionlessExt/ExceptionlessMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Exceptionless.Plugins;
+
+namespace Exceptionless.AspNetCore {
+    public class ExceptionlessMiddleware {
+        private readonly ExceptionlessClient _client;
+        private readonly RequestDelegate _next;
+
+        public ExceptionlessMiddleware(RequestDelegate next, ExceptionlessClient client) {
+            _client = client ?? ExceptionlessClient.Default;
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context) {
+            try {
+                await _next(context);
+            } catch (Exception ex) {
+                var contextData = new ContextData();
+                contextData.MarkAsUnhandledError();
+                contextData.SetSubmissionMethod(nameof(ExceptionlessMiddleware));
+
+                ex.ToExceptionless(contextData, _client).SetHttpContext(context).Submit();
+                throw;
+            }
+
+            if (context.Response?.StatusCode == 404) {
+                string path = context.Request.Path.HasValue ? context.Request.Path.Value : "/";
+                _client.CreateNotFound(path).SetHttpContext(context).Submit();
+            }
+        }
+    }
+}
---
+++ OYW.OA.Web/OYW.OA.Web/Exceptionless/OYWExceptionlessMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using Exceptionless;
+using Exceptionless.Plugins;
+using Microsoft.AspNetCore.Http;
+using OYW.OA.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OYW.OA.Web.Exceptionless
+{
+    public class OYWExceptionlessMiddleware
+    {
+        private readonly ExceptionlessClient _client;
+        private readonly RequestDelegate _next;
+
+        public OYWExceptionlessMiddleware(RequestDelegate next, ExceptionlessClient client)
+        {
+            _client = client ?? ExceptionlessClient.Default;
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                var contextData = new ContextData();
+                contextData.MarkAsUnhandledError();
+                contextData.SetSubmissionMethod(nameof(OYWExceptionlessMiddleware));
+                if (!ex.GetType().IsSubclassOf(typeof(OYWException)))
+                {
+                    ex.ToExceptionless(contextData, _client).SetHttpContext(context).Submit();
+                }
+                context.Response.ContentType = "text/plain;charset=utf-8";
+                await context.Response.WriteAsync(ex.Message);
+            }
+
+            if (context.Response?.StatusCode == 404)
+            {
+                string path = context.Request.Path.HasValue ? context.Request.Path.Value : "/";
+                _client.CreateNotFound(path).SetHttpContext(context).Submit();
+            }
+        }
+    }
+}
