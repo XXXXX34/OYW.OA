2aa7ad251d728012e0087f6a3e374b4920e75f02
XXXXX34 <503025565@qq.com>
2018-08-09 22:39:55 +0800

    Test

--- OYW.OA.Web/OYW.OA.Web/Controllers/MenuController.cs
+++ OYW.OA.Web/OYW.OA.Web/Controllers/MenuController.cs
@@ -20,6 +20,8 @@ namespace OYW.OA.Web.Controllers
         /// 
         public IActionResult GetMenus()
         {
+            var i = 100;
+
             var result = MenuService.GetMenus();
             return Json(new
             {
